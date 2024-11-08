bb8778d30470ee4edcb615423130df042790dd5b
John Paulo <johnpaulo0401@gmail.com>
2024-11-08 16:36:34 -0300

    Subindo ajustes dos assets finais, removido placeholders e algumas linhas de códigos desnecessárias, - para mim o código podia melhorar...

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let config = {
     default: "arcade",
     arcade: {
       gravity: { y: 600 },
-      debug: true,
+      debug: false,
     },
   },
   scene: {
@@ -49,36 +49,21 @@ function preload() {
   this.load.image("spaceship9", "assets/sprites/nave/tile008.png");
   this.load.image("spaceship010", "assets/sprites/nave/tile009.png");
   this.load.image("spaceship011", "assets/sprites/nave/tile010.png");
-  this.load.image("explosion1", "assets/sprites/explosion/explosion-e1.png");
-  this.load.image("explosion2", "assets/sprites/explosion/explosion-e2.png");
-  this.load.image("explosion3", "assets/sprites/explosion/explosion-e3.png");
-  this.load.image("explosion4", "assets/sprites/explosion/explosion-e4.png");
-  this.load.image("explosion5", "assets/sprites/explosion/explosion-e5.png");
-  this.load.image("explosion6", "assets/sprites/explosion/explosion-e6.png");
-  this.load.image("explosion7", "assets/sprites/explosion/explosion-e7.png");
-  this.load.image("explosion8", "assets/sprites/explosion/explosion-e8.png");
-  this.load.image("explosion9", "assets/sprites/explosion/explosion-e9.png");
-  this.load.image("explosion010", "assets/sprites/explosion/explosion-e10.png");
-  this.load.image("explosion011", "assets/sprites/explosion/explosion-e11png");
-  this.load.image("explosion012", "assets/sprites/explosion/explosion-e12.png");
-  this.load.image("explosion013", "assets/sprites/explosion/explosion-e13.png");
-  this.load.image("explosion014", "assets/sprites/explosion/explosion-e14.png");
-  this.load.image("explosion015", "assets/sprites/explosion/explosion-e15.png");
-  this.load.image("explosion016", "assets/sprites/explosion/explosion-e16.png");
-  this.load.image("explosion017", "assets/sprites/explosion/explosion-e17.png");
-  this.load.image("explosion018", "assets/sprites/explosion/explosion-e18.png");
-  this.load.image("explosion019", "assets/sprites/explosion/explosion-e19.png");
-  this.load.image("explosion020", "assets/sprites/explosion/explosion-e20.png");
-  this.load.image("explosion021", "assets/sprites/explosion/explosion-e21.png");
-  this.load.image("explosion022", "assets/sprites/explosion/explosion-e22.png");
-  this.load.image("pillar", "assets/sprites/pipe-green.png");
+  this.load.image("explosion1", "assets/sprites/explosion/explosion-f1.png");
+  this.load.image("explosion2", "assets/sprites/explosion/explosion-f2.png");
+  this.load.image("explosion3", "assets/sprites/explosion/explosion-f3.png");
+  this.load.image("explosion4", "assets/sprites/explosion/explosion-f4.png");
+  this.load.image("explosion5", "assets/sprites/explosion/explosion-f5.png");
+  this.load.image("explosion6", "assets/sprites/explosion/explosion-f6.png");
+  this.load.image("explosion7", "assets/sprites/explosion/explosion-f7.png");
+  this.load.image("explosion8", "assets/sprites/explosion/explosion-f8.png");
+  this.load.image("pillar", "assets/sprites/pipe.png");
   this.load.image("base", "assets/sprites/base.png");
   this.load.image("gameover", "assets/UI/gameover.png");
   this.load.image("score", "assets/UI/score.png");
-  this.load.image("retry", "assets/UI/retry.png");
-  this.load.image("startGame", "assets/UI/message.png");
+  this.load.image("retry", "assets/UI/replay.png");
+  this.load.image("startGame", "assets/UI/telainicial.png");
   this.load.image('cursor', 'assets/sprites/drawcursor.png');
-  this.load.audio("spaceshipSound", "assets/sfx/engine_run.mp3");
   this.load.audio("simplePoint", "assets/sfx/positive.ogg");
   this.load.audio("specialPoint", "assets/sfx/achievement.wav");
   this.load.audio("music2", "assets/music/music2.mp3");
@@ -98,14 +83,13 @@ function create() {
   base.setDepth(1);
   let startGameImage = this.add.image(game.config.width / 2, game.config.height / 3, "startGame");
   startGameImage.setOrigin(0.5, 0.5);
+  startGameImage.setDepth(2);
   character = this.physics.add.sprite(game.config.width / 4, game.config.height / 2, "spaceship1");
   character.setDepth(1);
   character.setCollideWorldBounds(true);
   character.body.allowGravity = false;
-
   backgroundMusic2 = this.sound.add("music2", { loop: true });
   backgroundMusic2.play();
-
   gameStart = false;
 
   this.anims.create({
@@ -126,6 +110,7 @@ function create() {
     frameRate:16,
     repeat:-1,
   });
+
   this.anims.create({
     key: "explosion",
     frames:[
@@ -137,22 +122,9 @@ function create() {
       {key:"explosion6"},
       {key:"explosion7"},
       {key:"explosion8"},
-      {key:"explosion9"},
-      {key:"explosion010"},
-      {key:"explosion011"},
-      {key:"explosion012"},
-      {key:"explosion013"},
-      {key:"explosion014"},
-      {key:"explosion015"},
-      {key:"explosion016"},
-      {key:"explosion017"},
-      {key:"explosion018"},
-      {key:"explosion019"},
-      {key:"explosion020"},
-      {key:"explosion021"},
-      {key:"explosion022"},
     ],
-    frameRate:26,
+    frameRate:9,
+    repeat:0,
   });
   character.setDisplaySize(100 ,100);
   character.setSize(200, 200);
@@ -162,11 +134,9 @@ function create() {
   this.input.on("pointerdown", function (pointer) {
     if (gameStart) return;
 
-    //sons aqui embaixo
     interlude = this.sound.add("music3", { loop: true });
     simplePoint = this.sound.add("simplePoint");
     specialPoint = this.sound.add("specialPoint");
-    //engine = this.sound.add("spaceshipSound", { loop: true });
 
     gameStart = true;
     startGameImage.setVisible(false);
@@ -178,15 +148,11 @@ function create() {
     this.physics.add.collider(character, this.lowerPillars, hitPillar, null, this);
     this.physics.add.collider(character, base, hitBase, null, this);
 
-    scoreText = this.add.text(game.config.width / 2, 30, "0", {
-      fontSize: "32px",
-      fontFamily: "Fantasy",
-      fill: "white",
-    });
+    scoreText = this.add.text(game.config.width / 2, 30, "0", {fontSize: "32px",fontFamily: "Verdana", fill: "#730c6e",});
     scoreText.setOrigin(0.5, 0.5);
     scoreText.setDepth(1);
 
-    var cursor = this.add.image(0, 0, 'cursor').setVisible(false);
+    let cursor = this.add.image(0, 0, 'cursor').setVisible(false);
     this.input.on('pointermove', function (pointer) {
       cursor.setPosition(pointer.x - 30, pointer.y);
       if (!isGameOver) {
@@ -266,7 +232,7 @@ function hitBase(character, base) {
   gameOverImage.setOrigin(0.5, 0.5);
   let scoreImage = this.add.image(game.config.width / 2, game.config.height, "score");
   scoreImage.setOrigin(0.5, 0.5);
-  finalScoreText = this.add.text(game.config.width / 2, game.config.height, score, { fontSize: "32px", fontFamily: "Fantasy", fill: "white" });
+  finalScoreText = this.add.text(game.config.width / 2, game.config.height, score, { fontSize: "32px", fontFamily: "Verdana", fill: "#730c6e" });
   finalScoreText.setOrigin(0.5, 0.5);
   this.tweens.add({
     targets: [scoreImage, finalScoreText],
@@ -288,7 +254,6 @@ function hitBase(character, base) {
     score = 0;
     gameStart = false;
     interlude.stop();
-    //this.sound.remove(engine);
     this.scene.restart();
     isRefresh = true;
   }, this);
